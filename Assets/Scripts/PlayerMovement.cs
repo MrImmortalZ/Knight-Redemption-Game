8288673eda4c6bbd428a79336b69f3badda07e9b
GigabyteZ <emaada21@iitk.ac.in>
2022-02-22 23:58:04 +0530

    Added animations and Normal sword attack (press Space to trigger)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,29 +8,28 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     public float runSpeed = 40f;
     bool jump = false;
-    bool crouch = false;
+    public Animator animator;
 
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         if(Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
 
         }
-        if(Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
+    
+    }
 
-        }else if(Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
-        }
+    public void OnLanding(){
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove*Time.fixedDeltaTime, crouch, jump);
+        controller.Move(horizontalMove*Time.fixedDeltaTime,false, jump);
         jump = false;
     }
 }
