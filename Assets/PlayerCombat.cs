14b043e6633cf626c6f849257d95cefb49cd68bd
Sarthak2099 <sarthaka21@iitk.ac.in>
2022-02-24 11:01:05 +0530

    cc

--- Assets/PlayerCombat.cs
+++ Assets/PlayerCombat.cs
@@ -4,26 +4,18 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    public float attackRange = 0.5f;
-    public int attackDamage = 40;
     public Animator animator;
     public Transform attackPoint;
+    public float attackRange = 0.5f;
     public LayerMask enemyLayers;
-    public float attackRate = 2f;
-    float nextAttackTime = 0f;
-
 
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time >= nextAttackTime)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-          if(Input.GetKeyDown(KeyCode.Space))
-          {
-              Attack();
-              nextAttackTime = Time.time + 1f/attackRate;
-          }
+            Attack();
         }
     }
     void Attack()
@@ -32,8 +24,7 @@ public class PlayerCombat : MonoBehaviour
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         foreach(Collider2D enemy in hitEnemies)
         {
-            //enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
-            Debug.Log("Enemy Hit");
+            Debug.Log("hit" + enemy.name);
         }
 
     }
